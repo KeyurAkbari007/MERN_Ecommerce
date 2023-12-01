39b54a2c4794103a078d5aecef49c4b30cb6b805
KeyurAkbari07 <keyur7051@gmail.com>
2023-12-01 17:01:58 +0530

    done

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const { User } = require("./model/User");
 const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
 const jwt = require("jsonwebtoken");
 const path = require("path");
+const { Order } = require("./model/Order");
 
 // const SECRET_KEY = "SECRET_KEY";
 
@@ -30,7 +31,7 @@ const endpointSecret = process.env.ENDPOINT_SECRET;
 server.post(
   "/webhook",
   express.raw({ type: "application/json" }),
-  (request, response) => {
+  async (request, response) => {
     let event = request.body;
     // Only verify the event if you have an endpoint secret defined.
     // Otherwise use the basic event deserialized with JSON.parse
@@ -52,10 +53,12 @@ server.post(
     // Handle the event
     switch (event.type) {
       case "payment_intent.succeeded":
-        const paymentIntent = event.data.object;
-        console.log(
-          `PaymentIntent for ${paymentIntent.amount} was successful!`
+        const paymentIntentSucceeded = event.data.object;
+        const order = await Order.findById(
+          paymentIntentSucceeded.metadata.orderId
         );
+        order.paymentStatus = "received";
+        await order.save();
         // Then define and call a method to handle the successful payment intent.
         // handlePaymentIntentSucceeded(paymentIntent);
         break;
@@ -101,6 +104,11 @@ server.use("/users", isAuth(), usersRouter.router);
 server.use("/auth", authRouter.router);
 server.use("/cart", isAuth(), cartRouter.router);
 server.use("/orders", isAuth(), orderRouter.router);
+
+// this line we add to make react router work in case of other routes doesnt match
+server.get("*", (req, res) =>
+  res.sendFile(path.resolve("build", "index.html"))
+);
 
 // passport strategies
 passport.use(
--- model/Order.js
+++ model/Order.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const paymentMethods = {
-  values: ['card', 'cash'],
-  message: 'enum validator failed for payment Methods'
-}
+  values: ["card", "cash"],
+  message: "enum validator failed for payment Methods",
+};
 const orderSchema = new Schema(
   {
     items: { type: [Schema.Types.Mixed], required: true },
     totalAmount: { type: Number },
     totalItems: { type: Number },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     paymentMethod: { type: String, required: true, enum: paymentMethods },
-    paymentStatus: { type: String, default: 'pending' },
-    status: { type: String, default: 'pending' },
+    paymentStatus: { type: String, default: "pending" },
+    status: { type: String, default: "pending" },
     selectedAddress: { type: Schema.Types.Mixed, required: true },
   },
   { timestamps: true }
 );
 
-const virtual = orderSchema.virtual('id');
+const virtual = orderSchema.virtual("id");
 virtual.get(function () {
   return this._id;
 });
-orderSchema.set('toJSON', {
+orderSchema.set("toJSON", {
   virtuals: true,
   versionKey: false,
   transform: function (doc, ret) {
@@ -31,4 +31,4 @@ orderSchema.set('toJSON', {
   },
 });
 
-exports.Order = mongoose.model('Order', orderSchema);
+exports.Order = mongoose.model("Order", orderSchema);
